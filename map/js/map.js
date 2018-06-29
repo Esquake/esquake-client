beb3e526687e891fddb8a92547b34740571cc5ad
Gihyeon Yang <dexterastin@gmail.com>
2018-06-29 09:16:36 +0900

    map: 생성자 인자 추가

--- map/js/map.js
+++ map/js/map.js
@@ -1,12 +1,13 @@
 class Map {
-    constructor() {
+    constructor(elementId) {
         this.map = new daum.maps.Map(
-            document.getElementById('map'),
+            document.getElementById(elementId),
             {
                 center: new daum.maps.LatLng(37.6, 127), //지도의 중심좌표.
                 level: 13
             }
         );
+
         this.map.setDraggable(true);
         this.map.setZoomable(true);
         this.geocoder = new daum.maps.services.Geocoder();
@@ -16,13 +17,19 @@ class Map {
         this.geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
     }
 
+    // TODO : 랜덤으로 좌표 생성하는 기능 추가
+    getRandomLocaton() {
+
+    }
+
     getRoadAddress() {
 
         // TODO : GPS에서 데이터 가져오는 기능 추가
 
         this.searchDetailAddrFromCoords(this.map.getCenter(), function (result, status) {
             if (status === daum.maps.services.Status.OK) {
-                this.current = result[0];
+                // return result;
+                console.log(result);
             }
         });
     }
@@ -30,6 +37,6 @@ class Map {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    let map = new Map();
+    let map = new Map('map');
     map.getRoadAddress();
 });
