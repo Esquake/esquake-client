c8c4bad04dd1a612d8296109dd31d247a78b3c76
inthewalter <21400564@handong.edu>
2018-07-06 14:22:41 +0900

    backend: crawler 수정, 모든 사람에게 push 가능

--- server/crawler.py
+++ server/crawler.py
@@ -1,3 +1,4 @@
+import requests
 import urllib.parse
 import urllib.request
 import xmltodict
@@ -24,7 +25,7 @@ class Crawler:
 
     def setTime(self):
         now = datetime.datetime.now()
-        self.param["fromTmFc"] = (now - datetime.timedelta(days=1)).strftime("%Y%m%d")
+        self.param["fromTmFc"] = (now - datetime.timedelta(days=3)).strftime("%Y%m%d")
         self.param["toTmFc"] = now.strftime("%Y%m%d")
         pass
 
@@ -46,22 +47,38 @@ class Crawler:
             target = self.url + "?" + urllib.parse.urlencode(self.param)
 
             result = urllib.request.urlopen(target).read().decode('utf-8')
+            #print(result)
             response = json.loads(json.dumps(xmltodict.parse(result)))
+            when = response["response"]["body"]["items"]["item"]["tmEqk"]
+            when2 = when[4:6] + "-" + when[6:8] + " " + when[8:10] + ":" + when[10:12]
+            mt = response["response"]["body"]["items"]["item"]["mt"]
+            loc = response["response"]["body"]["items"]["item"]["loc"]
+            title = when2 + " " + loc + " 규모 " + mt
+            if float(mt) <= 3.0 :
+                content = "눌러서 가까운 대피소를 확인하세요."
+            else :
+                content = "눌러서 가장 가까운 대피소로 이동하세요."
 
+            payload = {"app_id": "0d931a3c-8465-40dd-abaf-d747c18b4465", "included_segments": ["All"],
+                        "headings": {"en": title}, "contents": {"en": content},
+                        "smallicon": "ic_launcher_APP.png"}
+
+            req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
             if response["response"]["body"]["items"] is None:
                 continue
                 pass
-
-            for item in response["response"]["body"]["items"]["item"]:
-                if not self.find(item['tmEqk']):
-                    self.earthquake_ref.add(item)
-                    print("Data insert success.")
-                    print(item)
+            tmEqk = response["response"]["body"]["items"]["item"]['tmEqk']
+            if tmEqk :
+                if not self.find(tmEqk) :
+                    self.earthquake_ref.add(response["response"]["body"]["items"]["item"])
+                    print("Data insert success")
+                    print(response["response"]["body"]["items"]["item"])
+                    req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
+                    print(req)
                     pass
                 else:
                     print("Data already exists.")
                     pass
-                pass
             pass
         pass
 
@@ -69,6 +86,8 @@ class Crawler:
 
 
 if __name__ == '__main__':
+    header = {"Content-Type": "application/json; charset=utf-8", "Authorization": "Basic MjZhZjM1ZjYtN2U4Ni00ZTgyLTkzMDctMmIzYmVmZmRiYmM5"}
+    #payload = {"app_id": "0d931a3c-8465-40dd-abaf-d747c18b4465", "include_player_ids": ["77d716d8-bfce-4e75-942e-134558299a78"], "headings": {"en": "English Title"}, "contents": {"en": "English Message"}}
     crawler = Crawler(
         "http://newsky2.kma.go.kr/service/ErthqkInfoService/EarthquakeReport",
         {
