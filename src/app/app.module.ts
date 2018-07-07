113d14c0391c352da2e6b4f0d7a2e1030920f820
inthewalter <21400564@handong.edu>
2018-07-07 20:39:48 +0900

    menu css 수정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
 import { KakaoMapsModule } from 'kakao-maps-sdk';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
